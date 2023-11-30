193556602a93a7ab009f2ca031dad7a81238ed9a
SeoungminKwon <tjdalsrnjs10@gmail.com>
2023-11-30 23:51:34 +0900

    Setting

---
+++ src/main/java/com/example/sbbmission3/SbbMission3Application.java
@@ -0,0 +1,13 @@
+package com.example.sbbmission3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbMission3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbMission3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sbbmission3/SbbMission3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbbmission3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbMission3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
