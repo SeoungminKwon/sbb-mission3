d6d89f72d98ea312958a7c29dccfe5d321b894c3
SeoungminKwon <tjdalsrnjs10@gmail.com>
2023-12-01 01:32:17 +0900

    2-5 Repository

---
+++ src/main/java/com/example/sbbmission3/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.example.sbbmission3;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository< Answer, Integer > {
+
+}
--- src/test/java/com/example/sbbmission3/SbbMission3ApplicationTests.java
+++ src/test/java/com/example/sbbmission3/SbbMission3ApplicationTests.java
@@ -1,13 +1,35 @@
 package com.example.sbbmission3;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbMission3ApplicationTests {
 
+    @Autowired
+    private QuestionRepository questionRepository;
+    @Autowired
+    private AnswerRepository answerRepository;
+
     @Test
-    void contextLoads() {
+    @Transactional
+    void testJpa() {
+        Optional< Question > oq = questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question question = oq.get();
+        List< Answer > answerList = question.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+
     }
 
 }
